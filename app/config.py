449ad8e25f238d8b086cf39c418fb9f44cce3aed
Shaquille Thomas <roreythomz26@gmail.com>
2025-05-04 23:43:42 -0500

    Changes made

--- app/config.py
+++ app/config.py
@@ -1,13 +1,18 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()  # load environment variables from .env if it exists.
+load_dotenv()
 
 class Config(object):
     """Base Config Object"""
     DEBUG = False
     SECRET_KEY = os.environ.get('SECRET_KEY', 'Som3$ec5etK*y')
     UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace('postgres://', 'postgresql://')
+    
+    DATABASE_URL = os.environ.get('DATABASE_URL', '')
+    if DATABASE_URL.startswith('postgres://'):
+        DATABASE_URL = DATABASE_URL.replace('postgres://', 'postgresql://')
+    
+    SQLALCHEMY_DATABASE_URI = DATABASE_URL
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
