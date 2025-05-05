829d21967ffdef089cb17bebae93f39084c832e0
Shaquille Thomas <roreythomz26@gmail.com>
2025-05-05 04:36:20 -0500

    Changes made

--- app/config.py
+++ app/config.py
@@ -8,11 +8,6 @@ class Config(object):
     DEBUG = False
     SECRET_KEY = os.environ.get('SECRET_KEY', 'Som3$ec5etK*y')
     UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER')
-    
-    DATABASE_URL = os.environ.get('DATABASE_URL', '')
-    if DATABASE_URL.startswith('postgres://'):
-        DATABASE_URL = DATABASE_URL.replace('postgres://', 'postgresql://')
-    
-    SQLALCHEMY_DATABASE_URI = DATABASE_URL
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace('postgres://', 'postgresql://')
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
