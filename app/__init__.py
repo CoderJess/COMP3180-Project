82e844c051a378201f8731fccc9dd9fd509fa961
Shaquille Thomas <roreythomz26@gmail.com>
2025-05-04 23:31:50 -0500

    Changes made

--- app/__init__.py
+++ app/__init__.py
@@ -12,15 +12,15 @@ CORS(app)
 
 app.config.from_object(Config)
 
+if 'UPLOAD_FOLDER' not in app.config or app.config['UPLOAD_FOLDER'] is None:
+    app.config['UPLOAD_FOLDER'] = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'uploads')
+
 if not os.path.exists(app.config['UPLOAD_FOLDER']):
     os.makedirs(app.config['UPLOAD_FOLDER'])
 
 db = SQLAlchemy(app)
-#db.init_app(app)
 
 migrate = Migrate(app, db)
 jwt = JWTManager(app)
 
 from app import views
-
-
